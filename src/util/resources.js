ce951a287b996ef51c140547d656da5ef10fa95a
aurora32s <aurora32s@naver.com>
2022-04-25 15:43:20 +0900

    Updates

--- src/util/resources.js
+++ src/util/resources.js
@@ -3,7 +3,7 @@
  */
 
 // images
-export const DOWN_ARROW = '../resources/image/down-arrow.png';
+export const DOWN_ARROW = '/src/resources/image/down-arrow.png';
 export const RIGHT_ARROW = '/src/resources/image/right-arrow.png';
 export const PLUS_PNG = '/src/resources/image/plus.png';
 export const TRASH_PNG = '/src/resources/image/trash.png';
